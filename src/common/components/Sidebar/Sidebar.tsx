24c130c21ed36582c031bca47b563f5ec0b4e4d4
Joseph <joseph161216@gmail.com>
2023-03-24 01:07:52 -0700

    Add orginaztion selector hardcoded

--- src/common/components/Sidebar/Sidebar.tsx
+++ src/common/components/Sidebar/Sidebar.tsx
@@ -4,9 +4,9 @@ const Sidebar = () => {
   return (
     <>
       <aside className="fixed top-0 left-0 w-64 h-screen" aria-label="Sidebar">
-        <div className="bg-gray-100 h-full px-3 py-4">
-          <ul className="space-y-4 mx-4">
-            <li>
+        <div className="flex flex-col bg-gray-50 h-full px-3 py-4">
+          <ul className="space-y-4">
+            <li className="ml-2">
               <img
                 className="object-contain h-14 w-28"
                 src="/dropbox_logo_2017.svg"
@@ -14,21 +14,42 @@ const Sidebar = () => {
               />
             </li>
             <li>
-              <a href="#">Files</a>
+              <a className="p-2 hover:text-blue-700" href="#">
+                Files
+              </a>
             </li>
             <li>
-              <a href="#">Sharing</a>
+              <a className="p-2 hover:text-blue-700" href="#">
+                Sharing
+              </a>
             </li>
             <li>
-              <a href="#">Recents</a>
+              <a className="p-2 hover:text-blue-700" href="#">
+                Recents
+              </a>
             </li>
             <li>
-              <a href="#">File requests</a>
+              <a className="p-2 hover:text-blue-700" href="#">
+                File requests
+              </a>
             </li>
             <li>
-              <a href="#">Deleted Files</a>
+              <a className="p-2 hover:text-blue-700" href="#">
+                Deleted Files
+              </a>
             </li>
           </ul>
+
+          <div className="mt-auto">
+            <div className="border border-slate-200" />
+            <div className="flex items-center justify-between px-2 py-4">
+              <div>
+                <div>Acme Inc</div>
+                <div>34 Members</div>
+              </div>
+              <div>{"<>"}</div>
+            </div>
+          </div>
         </div>
       </aside>
     </>
